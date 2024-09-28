7aad5928ca0b98520c374c71812a72f3d313e2db
unknown <joe2@gmail.com>
2024-09-28 13:18:56 -0500

    1667. Fix Names in a Table

---
+++ 1667. Fix Names in a Table/1667. Fix Names in a Table.js
@@ -0,0 +1,4 @@
+    # Write your MySQL query statement below
+
+    select user_id, concat(UPPER(SUBSTRING(name,1,1)) ,LOWER(SUBSTRING(name,2,length(name)))) as name
+    from users order by user_id
